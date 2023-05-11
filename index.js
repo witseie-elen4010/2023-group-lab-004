cf0dd1335f751c2f458b820fb95f34f3cf790a6c
AhmadMoolla <ahmad@hoosen.com>
2023-05-11 17:04:55 +0200

    Fixing index again

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const mainRouter = require('./mainRoutes.js')
 
 app.use('/ ', mainRouter)
 
-app.listen(3000)
+const port = process.env.PORT || 3000
+app.listen(port)
 console.log('Express server running on port 3000')
