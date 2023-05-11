36bd66828d27954194c0b4c3df8e59c4ee7700c5
AhmadMoolla <ahmad@hoosen.com>
2023-05-11 22:46:33 +0200

    Modified app.listen

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const mainRouter = require('./mainRoutes')
 
 app.use(mainRouter)
 
-
-app.listen(3000)
+const port = process.env.PORT || 3000
+app.listen(port)
 console.log('Express server running on port 3000')
