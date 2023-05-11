c5af308885f48abfb3a27887dbe4adb6b1d92ab2
AhmadMoolla <ahmad@hoosen.com>
2023-05-11 17:03:14 +0200

    changes to index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 const express = require('express')
 const app = express()
-const mainRouter = require('./mainRoutes')
+const mainRouter = require('./mainRoutes.js')
 
-app.use(mainRouter)
+app.use('/ ', mainRouter)
 
 app.listen(3000)
 console.log('Express server running on port 3000')
