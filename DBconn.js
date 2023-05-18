e465b300e58076cbae20b3ed6eac961a2300667b
AhmadMoolla <ahmad@hoosen.com>
2023-05-18 19:53:26 +0200

    removed more commented out stuff

--- DBconn.js
+++ DBconn.js
@@ -20,39 +20,7 @@ conn.connect(
       throw err
     } else {
       console.log('Connection established.')
-      // queryDatabase()
     }
   })
 
 module.exports = conn
-
-// function queryDatabase () {
-//   conn.query('DROP TABLE IF EXISTS inventory;', function (err, results, fields) {
-//     if (err) throw err
-//     console.log('Dropped inventory table if existed.')
-//   })
-//   conn.query('CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);',
-//     function (err, results, fields) {
-//       if (err) throw err
-//       console.log('Created inventory table.')
-//     })
-//   conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['banana', 150],
-//     function (err, results, fields) {
-//       if (err) throw err
-//       else console.log('Inserted ' + results.affectedRows + ' row(s).')
-//     })
-//   conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['orange', 154],
-//     function (err, results, fields) {
-//       if (err) throw err
-//       console.log('Inserted ' + results.affectedRows + ' row(s).')
-//     })
-//   conn.query('INSERT INTO inventory (name, quantity) VALUES (?, ?);', ['apple', 100],
-//     function (err, results, fields) {
-//       if (err) throw err
-//       console.log('Inserted ' + results.affectedRows + ' row(s).')
-//     })
-//   conn.end(function (err) {
-//     if (err) throw err
-//     else console.log('Done.')
-//   })
-// };
